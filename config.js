e1db51a01f45fd0f77fe1443864bea8c1179b475
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-27 11:37:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Cb4kBIxD#f8uTrHRxHgxZdrr6wgzIgtbI88FHS_zkVzllz2xqlH0' : process.env.SESSION_ID,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'true' : process.env.ONLY_ME,
 ADDRESSES: process.env.ADDRESSES === undefined ? '' : process.env.ADDRESSES,
 ALIVE: process.env.ALIVE === undefined ? `Hello im alive now !!` : process.env.ALIVE,
-OWNER: process.env.OWNER === undefined ? `94725881990` : process.env.OWNER,
-PREFIX: process.env.PREFIX === undefined ? '@' : process.env.PREFIX,
+OWNER: process.env.OWNER === undefined ? `94775228949` : process.env.OWNER,
+PREFIX: process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
 FOOTER: process.env.FOOTER=== undefined ? '‌ꜱᴀᴄʜɪʙᴏᴛ': process.env.FOOTER,
 LOGO: process.env.LOGO === undefined ? `https://telegra.ph/file/f25fa1b13e9962f2c2271.jpg` : process.env.LOGO
 };
